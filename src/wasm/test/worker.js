a0bdef4c7577a9625df0e4b08d82132423cdf990
ruescasd <david@sequentech.io>
2022-06-06 17:18:19 +0200

    Fixed worker.js, back to modules

--- src/wasm/test/worker.js
+++ src/wasm/test/worker.js
@@ -1,14 +1,12 @@
-importScripts("../../../pkg/strand.js");
-const { test, bench } = wasm_bindgen;
-
-wasm_bindgen("../../../pkg/strand_bg.wasm").then(_ => {
+import * as pkg from "../../../pkg_t/strand.js";
+pkg.default().then(_ => {
     var parameters = {}
     location.search.slice(1).split("&").forEach( function(key_value) { var kv = key_value.split("="); parameters[kv[0]] = kv[1]; });
     postMessage(`Initialized wasm`);
-    test();
+    pkg.test();
     postMessage(' ');
     if(parameters['bench'] === 'true') {
-        bench(10);
+        pkg.bench(10);
     }
     postMessage(' ');
     postMessage('ok');
