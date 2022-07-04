0db9a5c73cf95c64da3834bb6f8c7a175d815150
ruescasd <david@sequentech.io>
2022-07-04 12:29:58 +0200

    Removed unused dependency

--- src/threshold.rs
+++ src/threshold.rs
@@ -149,7 +149,7 @@ impl<C: Ctx> KeymakerT<C> {
             }
             let present_exp = ctx.exp_from_u64(*p as u64);
             // we add exp_modulus to avoid calculating a modulus with negative numbers
-            // which can be implementation specific (also, the num_bigint backend does not support it)
+            // whose behaviour can be implementation specific (also, the num_bigint backend does not support it)
             let diff_exp = present_exp
                 .add(ctx.exp_modulus())
                 .sub(&trustee_exp)
