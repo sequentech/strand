005dbf82d879551604c6f7edf0a214c2fffaf97f
Félix Robles <felix@sequentech.io>
2022-07-06 09:04:44 +0100

    Nix build for wasm (#35)
    * wip nix
    
    * wip working nix build
    
    * fix cargo lock, more or less..
    
    * fixes
    
    * cargo lock
    
    * small readme update
    
    * using nixos-22.05 instead of nixos-21.11
    
    * fix cargo.lock.copy
    
    * using nixos-22.05 instead of nixos-21.11
    
    Co-authored-by: Eduardo Robles Elvira <edu@sequentech.io>

--- flake.nix
+++ flake.nix
@@ -1,67 +1,75 @@
-# SPDX-FileCopyrightText: 2022 Eduardo Robles <edulix@sequentech.io>
+# SPDX-FileCopyrightText: 2021 Eduardo Robles <edulix@sequentech.io>
 #
 # SPDX-License-Identifier: AGPL-3.0-only
-
 {
-  description = "Flake for test rust code";
+  description = "Flake to test rust code";
 
-  # input
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  # output function of this flake
+  
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let 
-          overlays = [ (import rust-overlay) ];
-          # pkgs is just the nix packages
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          rust-nightly = pkgs
-            .rust-bin
-            .selectLatestNightlyWith(
-              toolchain: toolchain.default.override {
-                extensions = [ "rust-src" ];
-                targets = [ "wasm32-unknown-unknown" ];
-              }
-            );
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { 
+          inherit system overlays;
+        };
+        stdenv = pkgs.clangStdenv;
+        rust-nightly = pkgs
+          .rust-bin
+          .nightly
+          ."2022-04-07"
+          .default
+          .override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+          }
+        ;
+      in rec {
+        defaultPackage = pkgs.rustPlatform.buildRustPackage {
+          pname = "strand";
+          version = "0.0.1";
+          src = ./.;
+          nativeBuildInputs = [
+            rust-nightly
+            pkgs.nodePackages.npm
+            pkgs.wasm-pack
+            pkgs.wasm-bindgen-cli
+          ];
+          buildPhase = ''
+            echo 'Build: wasm-pack build'
+            wasm-pack build --mode no-install --out-name index --release --target web --features=wasmtest
+          '';
+          installPhase = "
+            # set HOME temporarily to fix npm pack
+            mkdir -p $out/temp_home
+            export HOME=$out/temp_home
+            echo 'Install: wasm-pack pack'
+            wasm-pack -v pack .
+            rm -Rf $out/temp_home
+            cp pkg/strand-*.tgz $out
+            ";
 
-        # resulting packages of the flake
-        in rec {
-          # WIP Derivation for strand
-          # Continue work here following https://srid.ca/rust-nix reference
-          packages.strand = pkgs.clangStdenv.mkDerivation {
-            name = "strand";
-            version = "0.0.1";
-            src = self;
-            type = "git"; 
-            submodules = "true";
-            nativeBuildInputs = [
-                rust-nightly
-                pkgs.wasm-pack
-                pkgs.wasm-bindgen-cli
-                pkgs.libiconv
-                pkgs.yarn
-                pkgs.cargo-edit
-            ];
-          };
-          # strand is the default package
-          defaultPackage = packages.strand;
+            # see https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#importing-a-cargolock-file-importing-a-cargolock-file
+            cargoLock = let
+                fixupLockFile = path: (builtins.readFile path);
+            in {
+                lockFileContents = fixupLockFile ./Cargo.lock.copy;
+            };
+            postPatch = ''
+                cp ${./Cargo.lock.copy} Cargo.lock
+            '';
+        };
 
-          # configure the dev shell
-          devShell = (
-            pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }
-          ) { 
-            buildInputs = 
-              packages.strand.nativeBuildInputs ++
-              [
-                pkgs.bash
-                pkgs.act
-              ];
-          };
-        }
+        # configure the dev shell
+        devShell = (
+          pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }
+        ) { 
+          buildInputs = 
+            defaultPackage.nativeBuildInputs ++
+            [ pkgs.bash ]; 
+        };
+      }
     );
 }
