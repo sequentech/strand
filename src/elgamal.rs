9ffd5ef1ea94952fa4170d1417b5f4d7d5abfa44
David Ruescas <david@sequentech.io>
2022-12-10 17:33:51 +0100

    Removed simd feature from curve dalek by default

--- src/elgamal.rs
+++ src/elgamal.rs
@@ -173,3 +173,128 @@ impl<C: Ctx> PrivateKey<C> {
         }
     }
 }
+
+/*
+
+Taken from hom.rs
+
+// SPDX-FileCopyrightText: 2021 David Ruescas <david@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+use crate::crypto::elgamal::{Ciphertext, PublicKey};
+use crate::crypto::group::{ChaumPedersen, Element, Exponent, Group};
+
+// we need to prove that equality of discrete logs for
+// a / g^value and b, ie
+// a / g^value = g1^x and b = g2^x
+// for some x, given a, b, g1, g2
+#[allow(dead_code)]
+fn prove_value<E: Element, G: Group<E>>(
+    pk: &PublicKey<E, G>,
+    secret: &E::Exp,
+    ciphertext: &Ciphertext<E>,
+    value: &E::Exp,
+    label: &[u8],
+) -> ChaumPedersen<E> {
+    let group = &pk.group;
+    let v1 = &ciphertext.b;
+    let v2 = ciphertext.a.div(&group.gmod_pow(value), &group.modulus());
+    let g1 = None;
+    let g2 = &pk.value;
+
+    group.cp_prove(secret, v1, &v2, g1, g2, label)
+}
+
+#[allow(dead_code)]
+fn verify_value<E: Element, G: Group<E>>(
+    proof: &ChaumPedersen<E>,
+    pk: &PublicKey<E, G>,
+    ciphertext: &Ciphertext<E>,
+    value: &E::Exp,
+    label: &[u8],
+) -> bool {
+    let group = &pk.group;
+    let v1 = &ciphertext.b;
+    let v2 = ciphertext.a.div(&group.gmod_pow(value), &group.modulus());
+    let g1 = None;
+    let g2 = &pk.value;
+
+    group.cp_verify(v1, &v2, g1, g2, proof, label)
+}
+
+// OPT 1 parallel
+// OPT 2 precompute table (this is already included by ristretto backend)
+#[allow(dead_code)]
+fn discrete_log<E: Element, G: Group<E>>(group: G, element: E, max: i32) -> Option<E::Exp> {
+    let mut exp: E::Exp = E::Exp::add_identity();
+    let one = &E::Exp::mul_identity();
+    for _ in 0..max {
+        let next = group.gmod_pow(&exp);
+        if next == element {
+            return Some(exp);
+        }
+        exp = exp.add(one);
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::crypto::hom::*;
+
+    use rand::Rng;
+    use rug::Integer;
+
+    use crate::crypto::backend::ristretto_b::*;
+    use crate::crypto::backend::rug_b::*;
+    use crate::crypto::group::Group;
+
+    #[test]
+    fn test_hom_discretelog() {
+        let max = 1000;
+        let mut rng = rand::thread_rng();
+
+        let group = RugGroup::default();
+        let exp_value = Integer::from(rng.gen_range(0, max + 1));
+        let v = group.gmod_pow(&exp_value);
+
+        let result = discrete_log(group, v, max + 1);
+        assert!(result.is_some());
+        assert_eq!(exp_value, result.unwrap());
+    }
+
+    #[test]
+    fn test_hom_prove_value() {
+        let group = RugGroup::default();
+        let sk = group.gen_key();
+        let pk = PublicKey::from(&sk.public_value, &group);
+
+        let plaintext = group.rnd_exp();
+        let r = group.rnd_exp();
+        let c = pk.encrypt_exponential(&plaintext, &r);
+
+        let proof = prove_value(&pk, &r, &c, &plaintext, &vec![]);
+        let verified = verify_value(&proof, &pk, &c, &plaintext, &vec![]);
+        assert!(verified);
+        let verified = verify_value(&proof, &pk, &c, &group.rnd_exp(), &vec![]);
+        assert!(!verified);
+
+        let group = RistrettoGroup;
+        let sk = group.gen_key();
+        let pk = PublicKey::from(&sk.public_value, &group);
+
+        let plaintext = group.rnd_exp();
+        let r = group.rnd_exp();
+        let c = pk.encrypt_exponential(&plaintext, &r);
+
+        let proof = prove_value(&pk, &r, &c, &plaintext, &vec![]);
+        let verified = verify_value(&proof, &pk, &c, &plaintext, &vec![]);
+        assert!(verified);
+        let verified = verify_value(&proof, &pk, &c, &group.rnd_exp(), &vec![]);
+        assert!(!verified);
+    }
+}
+
+
+*/
